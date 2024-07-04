f61e8fd0fb12c8233cbcf04ff205c9bf47ca1452
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-07-05 00:34:53 +0530

    Create const.py

---
+++ Plotium/AlkalineEarthMetals/const.py
@@ -0,0 +1,55 @@
+
+sec_name = "Alkali Earth Metals"
+
+electronegativity = {
+    "Beryllium (Be)": 1.57,
+    "Magnesium (Mg)": 1.31,
+    "Calcium (Ca)": 1.00,
+    "Strontium (Sr)": 0.95,
+    "Barium (Ba)": 0.89,
+    "Radium (Ra)": 0.9
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Beryllium (Be)": 1.53,
+    "Magnesium (Mg)": 1.73,
+    "Calcium (Ca)": 2.31,
+    "Strontium (Sr)": 2.49,
+    "Barium (Ba)": 2.68,
+    "Radium (Ra)": 2.83
+}
+
+
+# density in g/cm^3
+density = {
+    "Beryllium (Be)": 1.85,
+    "Magnesium (Mg)": 1.74,
+    "Calcium (Ca)": 1.54,
+    "Strontium (Sr)": 2.64,
+    "Barium (Ba)": 3.62,
+    "Radium (Ra)": 5
+}
+
+
+# in Kelvin
+melting_point = {
+    "Beryllium (Be)": 1560,
+    "Magnesium (Mg)": 923,
+    "Calcium (Ca)": 1115,
+    "Strontium (Sr)": 1050,
+    "Barium (Ba)": 1000,
+    "Radium (Ra)": 969
+}
+
+
+# in K
+boiling_point = {
+    "Beryllium (Be)": 2741,
+    "Magnesium (Mg)": 1363,
+    "Calcium (Ca)": 1757,
+    "Strontium (Sr)": 1650,
+    "Barium (Ba)": 2118,
+    "Radium (Ra)": 1773
+}
+
