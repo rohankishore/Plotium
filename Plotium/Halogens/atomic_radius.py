90bf8674fa2f710d4e7d4f7f511c0fd0285213d5
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-07-05 00:55:10 +0530

    Halogens

---
+++ Plotium/Halogens/atomic_radius.py
@@ -0,0 +1,16 @@
+from const import atomic_radii, sec_name
+import matplotlib.pyplot as plt
+
+values = list(atomic_radii.values())
+elements = list(atomic_radii.keys())
+
+
+def plot():
+    plt.figure(figsize=(10, 5))
+    plt.plot(elements, values, color='skyblue')
+    plt.xlabel(f'{sec_name}')
+    plt.ylabel('Atomic Radii (van der Waals) (in pm)')
+    plt.title(f'Atomic Radii of {sec_name}')
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    return plt
