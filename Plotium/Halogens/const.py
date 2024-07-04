f03ae66f007a7c9974a500851e1b294f3c5aca64
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-07-05 00:54:53 +0530

    Create const.py

---
+++ Plotium/Halogens/const.py
@@ -0,0 +1,45 @@
+sec_name = "Halogens"
+
+electronegativity = {
+    "Fluorine (F)": 3.98,
+    "Chlorine (Cl)": 3.16,
+    "Bromine (Br)": 2.96,
+    "Iodine (I)": 2.66,
+    "Astatine (At)": 2.2
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Fluorine (F)": 135,
+    "Chlorine (Cl)": 175,
+    "Bromine (Br)": 183,
+    "Iodine (I)": 198,
+    "Astatine (At)": 202
+}
+
+# in Kelvin
+melting_point = {
+    "Fluorine (F)": 53.53,
+    "Chlorine (Cl)": 171.65,
+    "Bromine (Br)": 265.95,
+    "Iodine (I)": 386.85,
+    "Astatine (At)": 575
+}
+
+# in K
+boiling_point = {
+    "Fluorine (F)": 85.03,
+    "Chlorine (Cl)": 239.11,
+    "Bromine (Br)": 331.95,
+    "Iodine (I)": 457.55,
+    "Astatine (At)": None  # unknown
+}
+
+# density in g/cm^3
+density = {
+    "Fluorine (F)": 0.001696,
+    "Chlorine (Cl)": 0.003214,
+    "Bromine (Br)": 3.11,
+    "Iodine (I)": 4.93,
+    "Astatine (At)": 7,
+}
