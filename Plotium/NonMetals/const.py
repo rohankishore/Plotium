ab1e9d9da5a91c30e57905d441e3d3741f308720
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-07-05 17:52:57 +0530

    Create const.py

---
+++ Plotium/NonMetals/const.py
@@ -0,0 +1,111 @@
+
+sec_name = "Non Metals"
+
+electronegativity = {
+    "Hydrogen (H)": 2.20,
+    "Carbon (C)": 2.55,
+    "Nitrogen (N)": 3.04,
+    "Oxygen (O)": 3.44,
+    "Phosphorus (P)": 2.19,
+    "Sulfur (S)": 2.58,
+    "Selenium (Se)": 2.55,
+    "Fluorine (F)": 3.98,
+    "Chlorine (Cl)": 3.16,
+    "Bromine (Br)": 2.96,
+    "Iodine (I)": 2.66,
+    "Astatine (At)": 2.2,
+    "Helium (He)": None,
+    "Neon (Ne)": None,
+    "Argon (Ar)": None,
+    "Krypton (Kr)": 3.00,
+    "Xenon (Xe)": 2.60,
+    "Radon (Rn)": 2.20
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Hydrogen (H)": 0.53,
+    "Carbon (C)": 0.67,
+    "Nitrogen (N)": 0.56,
+    "Oxygen (O)": 0.48,
+    "Phosphorus (P)": 0.98,
+    "Sulfur (S)": 0.88,
+    "Selenium (Se)": 1.03,
+    "Fluorine (F)": 0.42,
+    "Chlorine (Cl)": 0.79,
+    "Bromine (Br)": 0.94,
+    "Iodine (I)": 1.15,
+    "Astatine (At)": 1.40,
+    "Helium (He)": 0.31,
+    "Neon (Ne)": 0.38,
+    "Argon (Ar)": 0.71,
+    "Krypton (Kr)": 0.88,
+    "Xenon (Xe)": 1.08,
+    "Radon (Rn)": 1.20
+}
+
+# density in g/cm^3
+density = {
+    "Hydrogen (H)": 0.00008988,
+    "Carbon (C)": 2.267,
+    "Nitrogen (N)": 0.0012506,
+    "Oxygen (O)": 0.001429,
+    "Phosphorus (P)": 1.82,
+    "Sulfur (S)": 2.07,
+    "Selenium (Se)": 4.79,
+    "Fluorine (F)": 0.001696,
+    "Chlorine (Cl)": 0.003214,
+    "Bromine (Br)": 3.12,
+    "Iodine (I)": 4.93,
+    "Astatine (At)": 7,
+    "Helium (He)": 0.0001786,
+    "Neon (Ne)": 0.0008999,
+    "Argon (Ar)": 0.001784,
+    "Krypton (Kr)": 0.003733,
+    "Xenon (Xe)": 0.005897,
+    "Radon (Rn)": 0.00973
+}
+
+# in Kelvin
+melting_point = {
+    "Hydrogen (H)": 14.01,
+    "Carbon (C)": 3823,
+    "Nitrogen (N)": 63.15,
+    "Oxygen (O)": 54.36,
+    "Phosphorus (P)": 317.3,
+    "Sulfur (S)": 388.36,
+    "Selenium (Se)": 494,
+    "Fluorine (F)": 53.48,
+    "Chlorine (Cl)": 171.6,
+    "Bromine (Br)": 265.8,
+    "Iodine (I)": 386.85,
+    "Astatine (At)": 575,
+    "Helium (He)": 0.95,
+    "Neon (Ne)": 24.56,
+    "Argon (Ar)": 83.81,
+    "Krypton (Kr)": 115.79,
+    "Xenon (Xe)": 161.36,
+    "Radon (Rn)": 202
+}
+
+# in K
+boiling_point = {
+    "Hydrogen (H)": 20.28,
+    "Carbon (C)": 4098,
+    "Nitrogen (N)": 77.36,
+    "Oxygen (O)": 90.20,
+    "Phosphorus (P)": 553.7,
+    "Sulfur (S)": 717.8,
+    "Selenium (Se)": 958,
+    "Fluorine (F)": 85.03,
+    "Chlorine (Cl)": 239.11,
+    "Bromine (Br)": 332.0,
+    "Iodine (I)": 457.4,
+    "Astatine (At)": 610,
+    "Helium (He)": 4.22,
+    "Neon (Ne)": 27.07,
+    "Argon (Ar)": 87.30,
+    "Krypton (Kr)": 119.93,
+    "Xenon (Xe)": 165.03,
+    "Radon (Rn)": 211.5
+}
