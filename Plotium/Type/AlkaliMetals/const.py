d90db58ce74ef8ad4a01e5beee2ce362638d0aa7
Rohan Kishore <rohankishore746@outlook.com>
2024-07-05 18:03:16 +0530

    Organized project structure into a new folder

---
+++ Plotium/Type/AlkaliMetals/const.py
@@ -0,0 +1,50 @@
+sec_name = "Alkali Metals"
+
+electronegativity = {
+    "Lithium (Li)": 0.98,
+    "Sodium (Na)": 0.93,
+    "Potassium (K)": 0.82,
+    "Rubidium (Rb)": 0.82,
+    "Cesium (Cs)": 0.79,
+    "Francium (Fr)": 0.7
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Lithium (Li)": 1.82,
+    "Sodium (Na)": 2.27,
+    "Potassium (K)": 2.75,
+    "Rubidium (Rb)": 3.03,
+    "Cesium (Cs)": 3.43,
+    "Francium (Fr)": 3.48
+}
+
+# density in g/cm^3
+density = {
+    "Lithium (Li)": 0.534,
+    "Sodium (Na)": 0.97,
+    "Potassium (K)": 0.89,
+    "Rubidium (Rb)": 1.53,
+    "Cesium (Cs)": 1.873,
+    "Francium (Fr)": None  # unknown
+}
+
+# in Kelvin
+melting_point = {
+    "Lithium (Li)": 453.65,
+    "Sodium (Na)": 370.944,
+    "Potassium (K)": 336.70,
+    "Rubidium (Rb)": 312.45,
+    "Cesium (Cs)": 301.70,
+    "Francium (Fr)": 294.00
+}
+
+# in K
+boiling_point = {
+    "Lithium (Li)": 1615.00,
+    "Sodium (Na)": 1156.090,
+    "Potassium (K)": 1032.00,
+    "Rubidium (Rb)": 961.00,
+    "Cesium (Cs)": 944.00,
+    "Francium (Fr)": 923.00
+}
