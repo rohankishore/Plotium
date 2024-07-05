5d079b462a6ce8ab2f959a0c04447ac9f4bbdcd6
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-07-05 17:48:26 +0530

    Create const.py

---
+++ Plotium/Lanthanoids/const.py
@@ -0,0 +1,96 @@
+
+sec_name = "Lanthanoids"
+
+electronegativity = {
+    "Lanthanum (La)": 1.10,
+    "Cerium (Ce)": 1.12,
+    "Praseodymium (Pr)": 1.13,
+    "Neodymium (Nd)": 1.14,
+    "Promethium (Pm)": 1.13,
+    "Samarium (Sm)": 1.17,
+    "Europium (Eu)": 1.20,
+    "Gadolinium (Gd)": 1.20,
+    "Terbium (Tb)": 1.10,
+    "Dysprosium (Dy)": 1.22,
+    "Holmium (Ho)": 1.23,
+    "Erbium (Er)": 1.24,
+    "Thulium (Tm)": 1.25,
+    "Ytterbium (Yb)": 1.10,
+    "Lutetium (Lu)": 1.27
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Lanthanum (La)": 1.87,
+    "Cerium (Ce)": 1.82,
+    "Praseodymium (Pr)": 1.83,
+    "Neodymium (Nd)": 1.82,
+    "Promethium (Pm)": 1.83,
+    "Samarium (Sm)": 1.81,
+    "Europium (Eu)": 2.04,
+    "Gadolinium (Gd)": 1.80,
+    "Terbium (Tb)": 1.78,
+    "Dysprosium (Dy)": 1.77,
+    "Holmium (Ho)": 1.76,
+    "Erbium (Er)": 1.76,
+    "Thulium (Tm)": 1.75,
+    "Ytterbium (Yb)": 1.94,
+    "Lutetium (Lu)": 1.73
+}
+
+# density in g/cm^3
+density = {
+    "Lanthanum (La)": 6.15,
+    "Cerium (Ce)": 6.77,
+    "Praseodymium (Pr)": 6.77,
+    "Neodymium (Nd)": 7.01,
+    "Promethium (Pm)": 7.26,
+    "Samarium (Sm)": 7.52,
+    "Europium (Eu)": 5.24,
+    "Gadolinium (Gd)": 7.90,
+    "Terbium (Tb)": 8.23,
+    "Dysprosium (Dy)": 8.55,
+    "Holmium (Ho)": 8.80,
+    "Erbium (Er)": 9.07,
+    "Thulium (Tm)": 9.32,
+    "Ytterbium (Yb)": 6.97,
+    "Lutetium (Lu)": 9.84
+}
+
+# in Kelvin
+melting_point = {
+    "Lanthanum (La)": 1193,
+    "Cerium (Ce)": 1071,
+    "Praseodymium (Pr)": 1208,
+    "Neodymium (Nd)": 1297,
+    "Promethium (Pm)": 1315,
+    "Samarium (Sm)": 1345,
+    "Europium (Eu)": 1095,
+    "Gadolinium (Gd)": 1585,
+    "Terbium (Tb)": 1629,
+    "Dysprosium (Dy)": 1680,
+    "Holmium (Ho)": 1734,
+    "Erbium (Er)": 1802,
+    "Thulium (Tm)": 1818,
+    "Ytterbium (Yb)": 1097,
+    "Lutetium (Lu)": 1936
+}
+
+# in K
+boiling_point = {
+    "Lanthanum (La)": 1193,
+    "Cerium (Ce)": 1071,
+    "Praseodymium (Pr)": 1208,
+    "Neodymium (Nd)": 1297,
+    "Promethium (Pm)": 1315,
+    "Samarium (Sm)": 1345,
+    "Europium (Eu)": 1095,
+    "Gadolinium (Gd)": 1585,
+    "Terbium (Tb)": 1629,
+    "Dysprosium (Dy)": 1680,
+    "Holmium (Ho)": 1734,
+    "Erbium (Er)": 1802,
+    "Thulium (Tm)": 1818,
+    "Ytterbium (Yb)": 1097,
+    "Lutetium (Lu)": 1936
+}
