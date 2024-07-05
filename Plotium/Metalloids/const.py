6b18b0df2a247ed154978bbbdcf0c5db6d17a389
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-07-05 17:50:30 +0530

    Create const.py

---
+++ Plotium/Metalloids/const.py
@@ -0,0 +1,51 @@
+
+sec_name = "Metalloids"
+
+electronegativity = {
+    "Boron (B)": 2.04,
+    "Silicon (Si)": 1.90,
+    "Germanium (Ge)": 2.01,
+    "Arsenic (As)": 2.18,
+    "Antimony (Sb)": 2.05,
+    "Tellurium (Te)": 2.10
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Boron (B)": 2.04,
+    "Silicon (Si)": 1.90,
+    "Germanium (Ge)": 2.01,
+    "Arsenic (As)": 2.18,
+    "Antimony (Sb)": 2.05,
+    "Tellurium (Te)": 2.10
+}
+
+# density in g/cm^3
+density = {
+    "Boron (B)": 2.34,
+    "Silicon (Si)": 2.33,
+    "Germanium (Ge)": 5.32,
+    "Arsenic (As)": 5.73,
+    "Antimony (Sb)": 6.68,
+    "Tellurium (Te)": 6.24
+}
+
+# in Kelvin
+melting_point = {
+    "Boron (B)": 2349,
+    "Silicon (Si)": 1687,
+    "Germanium (Ge)": 1211,
+    "Arsenic (As)": 1090,
+    "Antimony (Sb)": 903.78,
+    "Tellurium (Te)": 722.66
+}
+
+# in K
+boiling_point = {
+    "Boron (B)": 4200,
+    "Silicon (Si)": 3538,
+    "Germanium (Ge)": 3106,
+    "Arsenic (As)": 887,
+    "Antimony (Sb)": 1860,
+    "Tellurium (Te)": 1261
+}
