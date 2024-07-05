1718231af17f69fb9079ed66db8a65bd68164ea6
Rohan Kishore <109947257+rohankishore@users.noreply.github.com>
2024-07-05 17:51:59 +0530

    Create const.py

---
+++ Plotium/NobleGases/const.py
@@ -0,0 +1,51 @@
+
+sec_name = "Noble Gases"
+
+electronegativity = {
+    "Helium (He)": None,
+    "Neon (Ne)": None,    # Electronegativity not defined for Neon, Helium and Argon
+    "Argon (Ar)": None,
+    "Krypton (Kr)": 3.00,
+    "Xenon (Xe)": 2.60,
+    "Radon (Rn)": 2.20
+}
+
+# atomic radii in Angstroms
+atomic_radii = {
+    "Helium (He)": 0.31,
+    "Neon (Ne)": 0.38,
+    "Argon (Ar)": 0.71,
+    "Krypton (Kr)": 0.88,
+    "Xenon (Xe)": 1.08,
+    "Radon (Rn)": 1.20
+}
+
+# density in g/cm^3
+density = {
+    "Helium (He)": 0.0001786,
+    "Neon (Ne)": 0.0008999,
+    "Argon (Ar)": 0.001784,
+    "Krypton (Kr)": 0.003733,
+    "Xenon (Xe)": 0.005897,
+    "Radon (Rn)": 0.00973
+}
+
+# in Kelvin
+melting_point = {
+    "Helium (He)": 0.95,
+    "Neon (Ne)": 24.56,
+    "Argon (Ar)": 83.81,
+    "Krypton (Kr)": 115.79,
+    "Xenon (Xe)": 161.36,
+    "Radon (Rn)": 202
+}
+
+# in K
+boiling_point = {
+    "Helium (He)": 4.22,
+    "Neon (Ne)": 27.07,
+    "Argon (Ar)": 87.30,
+    "Krypton (Kr)": 119.93,
+    "Xenon (Xe)": 165.03,
+    "Radon (Rn)": 211.5
+}
